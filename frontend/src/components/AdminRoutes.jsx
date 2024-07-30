b83a40c9a6425c55c1362844f3f027d2de6aea03
Arnav_singh <avsingh02as02@gmail.com>
2024-07-30 18:35:20 +0530

    extracted frontend

--- frontend/src/components/AdminRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from 'react-router-dom';
-import {useSelector} from "react-redux";
-
-const AdminRoutes = () => {
-const {userInfo} = useSelector(state => state.auth)
-  return userInfo && userInfo.isAdmin ? <Outlet/> : <Navigate to="/login" replace/>
-};
-
-export default AdminRoutes;
